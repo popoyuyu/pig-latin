e3ef587af8019b77c8b856cd45995d4f7acb40e4
Jeff Lai <pjefflai@gmail.com>
2021-10-27 14:47:02 -0700

    move consonants up to vowel to the end of word and add 'ay'
    Co-authored-by: Drew Henderson <drewhendersonemail@gmail.com>
    Co-authored-by: Kim Brannian <brannian@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,21 +6,35 @@ function findVowel(word) {
   return false;
 }
 
+function isConsonant(word) {
+  let letters = word.split('');
+  let num = 0;
+  for (i = 0; i < word.length && num === 0; i++) {
+    if (findVowel(letters[0])) {
+      num = 1;
+      letters.push('a', 'y');
+    }
+    else {
+      letters.push(letters[0]);
+      letters.shift();
+    }
+  }
+  word = letters.join('');
+  return word;
+}
+
 function ifVowel(sentence) {
   let words = sentence.split(' ');
   words.forEach(function (word) {
     if (findVowel(word)) {
       word = word.concat("way");
+      console.log(word);
     }
     else {
-      let letters = word.split('');
-      letters.push(letters[0], 'a', 'y');
-      letters.shift();
-      console.log(letters);
-      word = letters.join('');
+      word = isConsonant(word);
+      console.log(word);
     }
-    console.log(word);
   });
 }
 
-ifVowel("hello this is a Test quiz");
+ifVowel("hello hi this is ");
