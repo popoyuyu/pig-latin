6a759c789fd701aa189aadbb1f95f569b798ab02
Jeff Lai <pjefflai@gmail.com>
2021-10-27 14:16:25 -0700

    3rd and 4th test of 'elloh' and 'ellohay' successful
    Co-authored-by: Drew Henderson <drewhendersonemail@gmail.com>
    Co-authored-by: Kim Brannian <brannian@gmail.com>

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 Describe: findVowel()
 
-Test: "hello this is a test"
-Code: findVowel("a, e, i, o, u");
+Test: "It will come out yes for words that begin with a vowel."
+Code: findVowel("hello this is a test");
 Expected Output: "yes"
 
 Test: "It will add 'way' to the end of words that begin with a vowel."
-Code: pigLatin("a");
-Expected Output: "away"
+Code: findVowel("hello this is a test");
+Expected Output: "isway, away"
+
+Describe: ifVowel()
+Test: "It will remove the first letter that is not a vowel to the end of the word.
+Code: ifVowel("hello this is a Test quiz");
+Expected Output: "elloh"
+
+Test: "It will join 'ay' to the first removed consonant."
+Code: findVowel("hello this is a Test quiz");
+Exepected Output: "ellohay"
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,26 @@
-function findVowel(sentence) {
+function findVowel(word) {
+  let letters = word.split('');
+  if (letters[0] === 'a' || letters[0] === 'e' || letters[0] === 'i' || letters[0] === 'o' || letters[0] === 'u' || letters[0] === 'A' || letters[0] === 'E' || letters[0] === 'I' || letters[0] === 'O' || letters[0] === 'U') {
+    return true;
+  }
+  return false;
+}
+
+function ifVowel(sentence) {
   let words = sentence.split(' ');
   words.forEach(function (word) {
-    let letters = word.split('');
-    console.log(letters[0]);
-    if (letters[0] === 'a' || letters[0] === 'e' || letters[0] === 'i' || letters[0] === 'o' || letters[0] === 'u' || letters[0] === 'A' || letters[0] === 'E' || letters[0] === 'I' || letters[0] === 'O' || letters[0] === 'U') {
+    if (findVowel(word)) {
       word = word.concat("way");
+    }
+    else {
+      let letters = word.split('');
+      letters.push(letters[0], 'a', 'y');
+      letters.shift();
+      console.log(letters);
+      word = letters.join('');
     }
     console.log(word);
   });
 }
 
-findVowel("hello this is a Test");
+ifVowel("hello this is a Test quiz");
